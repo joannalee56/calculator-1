4f10d107f856a7b88b7d7f1ce4143839006526cf
Joanna Lee <joanna.lee56@gmail.com>
2021-12-01 11:20:08 -0800

    Completed square function

--- arithmetic.py
+++ arithmetic.py
@@ -17,10 +17,9 @@ def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
     return num1 / num2
 
-
 def square(num1):
     """Return the square of num1."""
-
+    return num1 * num1
 
 def cube(num1):
     """Return the cube of num1."""
